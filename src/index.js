eb986a07a4b271fb7a47ef70f811e2d3a91baf81
Thomas <hakerh403@gmail.com>
2020-08-22 05:44:03 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,51 +3,208 @@
 const fs = require('fs');
 const path = require('path');
 const O = require('omikron');
-const FSEntry = require('./fs-entry');
-const types = require('./types');
 
-const allowUnsupported = 0;
-
-const getType = pth => {
-  const stat = fs.statSync(pth);
-
-  if(stat.isFile()) return types.FILE;
-  if(stat.isDirectory()) return types.DIRECTORY;
-  if(allowUnsupported) return types.UNKNOWN;
-
-  throw new TypeError('Unsupported file system entry type');
+const flags = {
+  cacheFileContent: 0,
 };
 
-const isFile = pth => getType(pth) === types.FILE;
-const isDir = pth => getType(pth) === types.DIRECTORY;
+const types = O.enum([
+  'DIR',
+  'FILE',
+  'OTHER',
+]);
 
-const iter = async (pth, func) => {
+class FSEntry extends O.Stringifiable{
+  constructor(pth){
+    super();
+
+    this.pth = norm(pth);
+
+    const info = path.parse(this.pth);
+    this.root = info.root;
+    this.dir = info.dir;
+    this.base = info.base;
+    this.name = info.name;
+    this.ext = info.ext.startsWith('.') ? info.ext.slice(1) : info.ext;
+  }
+
+  get exists(){
+    return fs.existsSync(this.pth);
+  }
+
+  get type(){ O.virtual('type'); }
+  get isDir(){ return this.type === types.DIR; }
+  get isFile(){ return this.type === types.FILE; }
+  get isOther(){ return this.type === types.OTHER; }
+
+  move(){ O.virtual('move'); }
+  moveRec(pth){ this.move(pth); }
+
+  copy(){ O.virtual('copy'); }
+  copyRec(pth){ this.copy(pth); }
+
+  delete(){ O.virtual('delete'); }
+  deleteRec(){ this.delete(); }
+}
+
+class Directory extends FSEntry{
+  #content = null;
+
+  get type(){ return types.FILE; }
+
+  get content(){
+    if(this.#content !== null)
+      return this.#content;
+
+    return this.#content = fs.readdirSync(this.pth).map(fileName => {
+      const pthNew = this.join(fileName);
+      const stat = fs.statSync(pthNew);
+
+      if(stat.isDirectory()) return new Directory(pthNew);
+      if(stat.isFile()) return new File(pthNew);
+      return new Other(pthNew);
+    });
+  }
+
+  join(pth){
+    return path.join(this.pth, pth);
+  }
+
+  rel(pth){
+    return path.relative(this.pth, pth);
+  }
+
+  move(pth){
+    fs.renameSync(this.pth, norm(pth));
+  }
+
+  copy(pth){
+    fs.mkdirSync(norm(pth));
+  }
+
+  copyRec(pth){
+    const pthOld = this.pth;
+    const pthNew = norm(pth);
+
+    this.topDown(entry  => {
+      const dest = path.join(pthNew, path.relative(pthOld, entry.pth));
+      entry.copy(dest);
+    });
+  }
+
+  delete(){
+    fs.rmdirSync(this.pth);
+  }
+
+  deleteRec(){
+    this.bottomUp(entry => {
+      entry.delete();
+    });
+  }
+
+  get chNum(){ return this.content.length; }
+  getCh(i){ return this.content[i]; }
+
+  toStr(){
+    const {content} = this;
+    if(content.length === 0) return this.name;
+
+    const arr = [this.name, this.inc, '\n'];
+    super.join(arr, content, '\n');
+    arr.push(this.dec);
+
+    return arr;
+  }
+}
+
+class File extends FSEntry{
+  static #buf = Buffer.alloc(1 << 16);
+  #content = null;
+
+  get type(){ return types.FILE; }
+
+  get content(){
+    if(this.#content !== null)
+      return this.#content;
+
+    const content = O.rfs(this.pth);
+
+    if(flags.cacheFileContent)
+      this.#content = content;
+
+    return content;
+  }
+
+  set content(content){
+    if(flags.cacheFileContent)
+      this.#content = content;
+
+    O.wfs(this.pth, content);
+  }
+
+  move(pth){
+    fs.renameSync(this.pth, norm(pth));
+  }
+
+  copy(pth){
+    const a = fs.openSync(this.pth, 'r');
+    const b = fs.openSync(norm(pth), 'w');
+
+    const buf = File.#buf;
+    const bufSize = buf.length;
+
+    while(1){
+      const len = fs.readSync(a, buf);
+      if(len === 0) break;
+
+      fs.writeSync(b, buf, 0, len);
+      if(len !== bufSize) break;
+    }
+
+    fs.closeSync(a);
+    fs.closeSync(b);
+  }
+
+  delete(){
+    fs.unlinkSync(this.pth);
+  }
+
+  get chNum(){ return 0; }
+
+  toStr(){
+    return this.base;
+  }
+}
+
+class Other extends FSEntry{
+  get type(){ return types.OTHER; }
+  get chNum(){ return 0; }
+
+  toStr(){
+    return this.base;
+  }
+}
+
+const norm = pth => {
   if(!path.isAbsolute(pth))
     pth = path.join(process.cwd(), pth);
+
   pth = path.normalize(pth);
 
-  const base = path.parse(pth).base;
-  const all = base === '*';
-  if(all) pth = path.join('..');
-
-  if(!fs.existsSync(pth))
-    throw new Error('Path does not exist');
-
-  const type = getType(pth);
-
-  if(type !== types.FILE){
-    const fse = new FSEntry(path.join(pth, '..'));
-    await func(fse);
-    return;
-  }
+  return pth;
 };
 
-const del = async pth => {};
-
 module.exports = {
-  FSEntry,
+  flags,
   types,
 
-  iter,
-  del,
+  FSEntry,
+  Directory,
+  File,
+
+  norm,
+
+  get cwd(){
+    return new Directory(process.cwd());
+  },
 };
